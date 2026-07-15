824eff01d432e4bb89c331eaf9cdc5d0ca0a442d
premot <126085020+premot@users.noreply.github.com>
2026-07-15 14:51:43 +0300

    Add current Ubuntu settings and setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while read -r schema key value; do
+  gsettings set "$schema" "$key" "$value"
+done < gsettings.txt
+
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+
+sudo tee /etc/gdm3/custom.conf >/dev/null <<EOF2
+[daemon]
+AutomaticLoginEnable=True
+AutomaticLogin=$USER
+EOF2
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+sudo systemctl enable --now ssh
