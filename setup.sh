326d7f8033fa0e72860740a4c848c991898e3323
premot <126085020+premot@users.noreply.github.com>
2026-07-17 18:39:14 +0300

    ok

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+dconf load / < gsettings2.txt
+
+exit 0
+
 while read -r schema key value; do
   gsettings set "$schema" "$key" "$value"
 done < gsettings.txt
+
+exit 0
 
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
