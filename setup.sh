d24b866a811142a23aa26eb3f510c24e87ac09e8
n@x <n@x>
2026-07-23 12:23:41 +0300

    this repo replaced fully by rice, can be discarded

--- setup.sh
+++ setup.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
-dconf load / < gsettings2.txt
+dconf load / < my.dconf
 
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
 
-
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 sudo systemctl enable --now ssh
+
+#exit 0 
 
 cd ~/
 rm Documents/ Music/ Pictures/ Templates/ Videos/ -rf
 
-
-exit 0
-
-#sudo tee /etc/gdm3/custom.conf >/dev/null <<EOF2
-#[daemon]
-#AutomaticLoginEnable=True
-#AutomaticLogin=$USER
-#EOF2
-
-#while read -r schema key value; do
-#  gsettings set "$schema" "$key" "$value"
-#done < gsettings.txt
-
+sudo tee /etc/gdm3/custom.conf >/dev/null <<EOF2
+[daemon]
+AutomaticLoginEnable=True
+AutomaticLogin=$USER
+EOF2
 
